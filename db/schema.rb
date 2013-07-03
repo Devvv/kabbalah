bb392990846608b17422a34be3cc1b07451b9661
Je <jetapahob@gmail.com>
2013-07-03 23:54:17 +0400

    devise 3

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130703114547) do
+ActiveRecord::Schema.define(version: 20130703195204) do
 
   create_table "articles", force: true do |t|
     t.string   "name",       default: ""
@@ -22,23 +22,21 @@ ActiveRecord::Schema.define(version: 20130703114547) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                              default: "", null: false
-    t.string   "encrypted_password",     limit: 128, default: "", null: false
+    t.string   "name",                   default: ""
+    t.string   "subname",                default: ""
+    t.string   "surname",                default: ""
+    t.text     "text",                   default: ""
+    t.integer  "active",                 default: 1
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "name",                               default: ""
-    t.string   "subname",                            default: ""
-    t.string   "surname",                            default: ""
-    t.text     "text",                               default: ""
-    t.integer  "active",                             default: 1
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
